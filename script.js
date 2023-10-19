335b852f5bb1e43a7e87ba3ba0ce0e5308491859
Preston Viloria <prestonviloria@Prestons-MacBook-Pro-70.local>
2023-10-19 00:45:00 -0600

    added second page

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 class Person {
     constructor({first_name, last_name}){
-        this.first = first_name;
-        this.last = last_name;
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
 }
 
 let names = [];
 
 function updateStorage(data){
-    let json = JSON.stringify(data);
-    localStorage.setItem('database', json);
+    //et json = JSON.stringify(data);
+    localStorage.setItem('database', JSON.stringify(data));
+    readStorage();
 }
 
 function readStorage(){
     let json = localStorage.getItem('database');
-    json_parse = JSON.parse(json) || [];
-    let result = json_parse.map(first_last => new Person(first_last));
+    let result = JSON.parse(json) || [];
+    result = result.map(first_last => new Person(first_last));
     return result;
 }
 
 function repeat_name(event){
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const first = formData.get('first_name');
-    const last = formData.get('last_name');
+    const first = formData.get("first_name");
+    const last = formData.get("last_name");
     const new_per = new Person({ first, last });
     let names = readStorage();
     names.push(new_per);
